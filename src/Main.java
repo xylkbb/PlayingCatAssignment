1233e53204d677db65746fd9b91d75cf95dcb6a3
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-29 13:29:32 -0400

    Commitial

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+    }
+
+    private static boolean isCatPlaying(boolean summer, int temperature) {
+        if(summer){
+            return temperature >= 25 && temperature <= 45;
+        }else{
+            return temperature >= 25 && temperature <= 35;
+        }
+    }
+
+
 }
